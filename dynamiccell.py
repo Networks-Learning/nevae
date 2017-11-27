541e16e03341acdf9c1654df2170559ae47b891a
Bidisha Samanta <bidisha@wks-51-310.mpi-sws.org>
2017-11-27 13:55:24 +0100

    Ver 0

---
+++ dynamiccell.py
@@ -0,0 +1,104 @@
+from layer import *
+import tensorflow as tf
+import numpy as np
+from utils import *
+from math import exp
+
+
+class VAEGDCell(tf.nn.rnn_cell.RNNCell):
+    """Variational Auto Encoder cell."""
+
+    def __init__(self, adj, features, h_dim, x_dim, z_dim):
+        '''
+        Args:
+        adj : adjacency matrix
+        features: feature matrix
+
+        hyperparameters
+        h_dim : hidden state dimension
+        x_dim : input chunk size (number of edges to be processed at the same time)
+        z_dim : encoded space dimension
+        '''
+
+        self.adj = adj
+        self.features = features
+        self.n_h = h_dim
+        self.n_x = x_dim
+        self.n_z = z_dim
+        self.n_x_1 = x_dim
+        self.n_z_1 = z_dim
+        self.n_enc_hidden = z_dim
+        self.n_dec_hidden = x_dim
+        self.n_prior_hidden = z_dim
+        self.name = self.__class__.__name__.lower()
+        self.lstm = tf.nn.rnn_cell.LSTM(self.n_h, state_is_tuple=True)
+
+    @property
+    def state_size(self):
+        return (self.n_h, self.n_h)
+
+    @property
+    def output_size(self):
+        return self.n_h
+
+    def __call__(self, state, c_x, n, d, k, eps_passed, sample, bias_laplace, scope=None):
+        '''
+		Args:
+			c_x - tensor to be filled up with random walk property
+			n - number of nodes
+			d - number of features in the feature matrix
+			k - length of random walk
+				defaults to be None
+    	'''
+        c , h = state
+        with tf.variable_scope(scope or type(self).__name__):
+            c_x = input_layer(c_x, self.adj, self.features, k, n, d, activation=None, batch_norm=False, istrain=False,
+                              scope=None)
+
+        with tf.variable_scope("Prior"):
+            prior_hidden = fc_layer(h, self.n_prior_hidden, activation=tf.nn.relu, scope="hidden")
+            prior_mu = fc_layer(prior_hidden, self.n_z, scope="mu")
+            prior_sigma = fc_layer(prior_hidden, self.n_z, activation=tf.nn.softplus, scope="sigma")  # >=0
+
+        c_x_1 = fc_layer(tf.matmul(c_x[-1],bias_laplace), self.n_x_1, activation = tf.nn.relu, scope = "phi_c")# >=0
+
+        with tf.variable_scope("Encoder"):
+                list_cx = tf.unstack(c_x)
+                list_cx.append(c_x_1)
+                list_cx.append(h)
+                # output will be of shape n X kd
+                enc_hidden = fc_layer(tf.concat(list_cx, 1), self.n_enc_hidden, activation=tf.nn.relu, scope="hidden")
+                # output will be of shape n X 5 (this is a hyper paramater)
+                enc_mu = fc_layer(enc_hidden, self.n_z, activation=tf.nn.softplus, scope='mu')
+                enc_mu = tf.reshape(enc_mu, [n, self.n_z, 1])
+
+                # output will be n X 1 then convert that to a diagonal matrix
+                intermediate_sigma = fc_layer(enc_hidden, self.n_z, activation=tf.nn.softplus, scope='sigma')
+                intermediate_sigma = tf.Print(intermediate_sigma, [intermediate_sigma], message="my debug_sigma-values:")
+                enc_sigma = tf.matrix_diag(intermediate_sigma, name="enc_sigma")
+
+
+        # Random sampling ~ N(0, 1)
+        eps = eps_passed
+        # tf.random_normal((n, 5, 1), 0.0, 1.0, dtype=tf.float32)
+
+        temp_stack = []
+        for i in range(n):
+            temp_stack.append(tf.matmul(enc_sigma[i], eps[i]))
+            z = tf.add(enc_mu, tf.stack(temp_stack))
+            # While we are trying to sample some edges, we sample Z from prior
+        if sample:
+            z = eps
+
+        with tf.variable_scope("Decoder"):
+                z_stack = []
+                for u in range(n):
+                    # Assuming the graph is undirected
+                    for v in range(u+1, n):
+                        z_stack.append(tf.concat(values=(tf.transpose(z[u]), tf.transpose(z[v])), axis=1)[0])
+                dec_hidden = fc_layer(tf.stack(z_stack), 1, activation=tf.nn.softplus, scope="hidden")
+
+        return (c_x, enc_mu, enc_sigma, dec_hidden, prior_mu, prior_sigma, z)
+
+    def call(self, state, c_x, n, d, k, eps_passed, sample, bias_laplace):
+        return self.__call__(state, c_x, n, d, k, eps_passed, sample, bias_laplace)
--- model.py
+++ model.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.DEBUG)
 
 
 class VAEG(VAEGConfig):
-    def __init__(self, hparams, placeholders, num_nodes, num_features, istest=False):
+    def __init__(self, hparams, placeholders, num_nodes, num_features, edges, istest=False):
         self.features_dim = num_features
         self.input_dim = num_nodes
         self.dropout = placeholders['dropout']
@@ -26,7 +26,7 @@ class VAEG(VAEGConfig):
         self.decay = placeholders['decay']
         self.n = num_nodes
         self.d = num_features
-
+        self.edges = edges
         #self.edges, self.non_edges = edges, non_edges
 
 
@@ -39,9 +39,8 @@ class VAEG(VAEGConfig):
             with tf.variable_scope('NLL'):
                 dec_mat = tf.exp(tf.minimum(tf.reshape(prob_dict, [self.n, self.n]),tf.fill([self.n, self.n], 10.0)))
                 dec_mat = tf.Print(dec_mat, [dec_mat], message="my decscore values:")
-
-
                 print "Debug dec_mat", dec_mat.shape, dec_mat.dtype, dec_mat
+
 		comp = tf.subtract(tf.ones([self.n, self.n], tf.float32), self.adj)
                 comp = tf.Print(comp, [comp], message="my comp values:")
 
@@ -90,12 +89,12 @@ class VAEG(VAEGConfig):
                 print "debug KL", first_term.shape, second_term.shape, k.shape, third_term.shape, sigma_1[0].shape
                 #return 0.5 *tf.reduce_sum((
                 return 0.5 * tf.add(tf.subtract(tf.add(first_term ,second_term), k), third_term)
-        
-	def get_lossfunc(enc_mu, enc_sigma, debug_sigma,prior_mu, prior_sigma, dec_out):
-            kl_loss = kl_gaussian(enc_mu, enc_sigma, debug_sigma,prior_mu, prior_sigma)  # KL_divergence loss
-            likelihood_loss = neg_loglikelihood(dec_out)  # Cross entropy loss
-            self.ll = likelihood_loss
-            return tf.reduce_mean(kl_loss + likelihood_loss)
+
+        def get_lossfunc(enc_mu, enc_sigma, debug_sigma,prior_mu, prior_sigma, dec_out):
+                kl_loss = kl_gaussian(enc_mu, enc_sigma, debug_sigma,prior_mu, prior_sigma)  # KL_divergence loss
+                likelihood_loss = neg_loglikelihood(dec_out)  # Cross entropy loss
+                self.ll = likelihood_loss
+                return tf.reduce_mean(kl_loss + likelihood_loss)
 
 
         self.adj = tf.placeholder(dtype=tf.float32, shape=[self.n, self.n], name='adj')
@@ -103,9 +102,9 @@ class VAEG(VAEGConfig):
         self.input_data = tf.placeholder(dtype=tf.float32, shape=[self.k, self.n, self.d], name='input')
         self.eps = tf.placeholder(dtype=tf.float32, shape=[self.n, 5, 1], name='eps')
 
-	self.cell = VAEGCell(self.adj, self.features)
+        self.cell = VAEGCell(self.adj, self.features)
         self.c_x, enc_mu, enc_sigma, debug_sigma,dec_out, prior_mu, prior_sigma, z_encoded = self.cell.call(self.input_data, self.n, self.d, self.k, self.eps, hparams.sample)
-	self.prob = dec_out
+        self.prob = dec_out
         self.z_encoded = z_encoded
         self.cost = get_lossfunc(enc_mu, enc_sigma, debug_sigma,prior_mu, prior_sigma, dec_out)
 
@@ -114,17 +113,8 @@ class VAEG(VAEGConfig):
         self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr)
         self.grad = self.train_op.compute_gradients(self.cost)
         self.grad_placeholder = [(tf.placeholder("float", shape=gr[1].get_shape()), gr[1]) for gr in self.grad]
-        #self.capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in self.grad]  
-        #self.tgv = [self.grad]
-        # self.apply_transform_op = self.train_op.apply_gradients(self.grad_placeholder)
-        #self.apply_transform_op = self.train_op.apply_gradients(self.capped_gvs)
         self.apply_transform_op = self.train_op.apply_gradients(self.grad)
-
-        #self.lr = tf.Variable(self.lr, trainable=False)
-        #self.gradient = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=1e-4).compute_gradients(self.cost)
-        #self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=1e-4).minimize(self.cost)
-        #self.check_op = tf.add_check_numerics_ops()
-	self.sess = tf.Session()
+        self.sess = tf.Session()
 
     def initialize(self):
         logger.info("Initialization of parameters")
@@ -137,11 +127,24 @@ class VAEG(VAEGConfig):
         print("Load the model from {}".format(ckpt.model_checkpoint_path))
         saver.restore(self.sess, ckpt.model_checkpoint_path)
 
+    def next_batch(self):
+        if self.current >= len(self.edges):
+            return False
+        adj = np.zeros([self.n, self.n])
+        for (u,v) in self.edges[: self.current + self.batch_size]:
+        #for (u,v) in edge_list:
+            adj[u][v] = 1
+            adj[v][u] = 1
+        bias_matrix = basis(self.adj)
+        self.current = self.current + self.batch_size
+        feature = calculate_feature(self.adj)
+        return (adj, bias_matrix, feature)
+
     def train(self,placeholders, hparams, adj, features):
         savedir = hparams.out_dir
         lr = hparams.learning_rate
         dr = hparams.dropout_rate
-	decay = hparams.decay_rate
+        decay = hparams.decay_rate
 
         # training
         num_epochs = hparams.num_epochs
@@ -156,18 +159,24 @@ class VAEG(VAEGConfig):
         iteration = 1
         #1000
         for epoch in range(num_epochs):
-            for i in range(len(adj)):
-
+            for batch in range(self.n_batches):
+            #for i in range(len(adj)):
+                adj, basis, features = self.next_batch()
                 # Learning rate decay
-                #self.sess.run(tf.assign(self.lr, self.lr * (self.decay ** epoch)))
+                self.sess.run(tf.assign(self.lr, self.lr * (self.decay ** epoch)))
+
                 feed_dict = construct_feed_dict(lr, dr, self.k, self.n, self.d, decay, placeholders)
-                feed_dict.update({self.adj: adj[i]})
-	        print "Debug", features[i].shape
-                eps = np.random.randn(self.n, 5, 1)  
-                #tf.random_normal((self.n, 5, 1), 0.0, 1.0, dtype=tf.float32)
-                feed_dict.update({self.features: features[i]})
-                feed_dict.update({self.input_data: np.zeros([self.k,self.n,self.d])})
+
+                # sampled random from standard normal distribution
+                eps = np.random.randn(self.n, self.n_z, 1)
                 feed_dict.update({self.eps: eps})
+
+                # The graph properties of the graph
+                feed_dict.update({self.adj: adj})
+                feed_dict.update({self.features: features})
+                feed_dict.update({self.input_data: np.zeros([self.k,self.n,self.d])})
+                feed_dict.update({self.basis: basis})
+
                 grad_vals = self.sess.run([g[0] for g in self.grad], feed_dict=feed_dict)
                 for j in xrange(len(self.grad_placeholder)):
                     feed_dict.update({self.grad_placeholder[j][0]: grad_vals[j]})
@@ -178,10 +187,13 @@ class VAEG(VAEGConfig):
                 #print "Debug CX", cx
                 if iteration % hparams.log_every == 0 and iteration > 0:
                     print("{}/{}(epoch {}), train_loss = {:.6f}".format(iteration, num_epochs, epoch + 1, train_loss))
-		    #print(probdict)
+		            #print(probdict)
                     checkpoint_path = os.path.join(savedir, 'model.ckpt')
                     saver.save(self.sess, checkpoint_path, global_step=iteration)
                     logger.info("model saved to {}".format(checkpoint_path))
+
+
+
 
     def plot_hspace(self, hparams, placeholders, num):
             #plot the coordinate in hspace
@@ -248,7 +260,7 @@ class VAEG(VAEGConfig):
         #tf.random_normal((self.n, 5, 1), 0.0, 1.0, dtype=tf.float32)
         feed_dict = construct_feed_dict(hparams.learning_rate, hparams.dropout_rate, self.k, self.n, self.d, hparams.decay_rate, placeholders)
         feed_dict.update({self.adj: adj})
-	feed_dict.update({self.features: deg})
+	    feed_dict.update({self.features: deg})
         feed_dict.update({self.input_data: np.zeros([self.k,self.n,self.d])})
         feed_dict.update({self.eps: eps})
         prob, ll = self.sess.run([self.prob, self.ll],feed_dict=feed_dict )
